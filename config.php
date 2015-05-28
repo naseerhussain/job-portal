f3f99be3d30b07e017c1d6dc6fd13e61d044bca9
naseerhussain <naseer.aman@gmail.com>
2015-05-28 19:45:44 +0530

    php files added with twitter login working

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+
+$CONSUMER_KEY='lMowAQ1ncC6bQEJIgHPXaNVPe';
+$CONSUMER_SECRET='RCrE56xy6oq8q4hfmUJTPzy3uZbWXPHCr99DlVcOGmGV7JmAMO';
+$OAUTH_CALLBACK='http://handbook.com/oauth.php';
+
+?>
--- getuser.php
+++ getuser.php
@@ -1,21 +1,31 @@
 <?PHP
-$connection = mysql_connect("localhost", "root", ""); // Establishing Connection with Server
-$db = mysql_select_db("Company", $connection); // Selecting Database from Server
-//if(isset($_POST['submit'])){ // Fetching variables of the form which travels in URL
-$CompName = $_GET['cName'];
+$conn = mysql_connect("localhost:3306", "root", "pwd"); // Establishing Connection with Server
+
+
+if(! $conn){
+	die("Connection failed: " . mysql_error());
+}
+
+
+$compName = $_GET['name'];
 $compDesc = $_GET['cDesc'];
 $compProduct = $_GET['cProd'];
 $skills = $_GET['skills'];
-if($CompName !=''||$prod !=''){
-//Insert Query of SQL
-$query = mysql_query("insert into Company(id,name,desp,prod,skill) values ('$CompName','$compDesc ','$compProduct','$skills')");
-echo "<br/><br/><span>Data Inserted successfully...!!</span>";
+$url = $_GET['url'];
+$category = $_GET['category'];
+
+
+$sql ="INSERT INTO company(id,companyName,description,category,product,skills,url) VALUES (NULL,'$compName','$compDesc','$category','$compProduct','$skills','$url')";
+mysql_select_db('handbook');
+$insert = mysql_query( $sql, $conn );
+
+if (! $insert) {
+	die('Could not enter data: ' . mysql_error());
+} else {
+    echo "Data entered successfully";
 }
-else{
-echo "<p>Insertion Failed <br/> Some Fields are Blank....!!</p>";
-}
-}
-mysql_close($connection); // Closing Connection with Server
+
+mysql_close($conn);
 ?>
 
 
